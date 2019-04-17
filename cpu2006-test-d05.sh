a8b339d628656932ea6c839b05359013eb6ba316
chenzhihui <chenzhihui244@msn.com>
2019-04-17 11:22:58 +0800

    fix invalid of report

--- cpu2006-test-d05.sh
+++ cpu2006-test-d05.sh
@@ -10,65 +10,57 @@ install_depency_ubuntu() {
 	apt install m4
 }
 
-mount | grep -q "$iso_file" > /dev/null 2>&1
-if [ ! $? -eq 0 ]; then
-	mount $iso_file $iso_mnt
-fi
-
-[ -d $cpu2006_dir ] || {
-	mkdir $cpu2006_dir && cp -a $iso_mnt/* $cpu2006_dir
-
-	# replace make
-	#rm -rf $cpu2006_dir/tools/src/make-3.82
-	#tar xf make.tar.gz -C $cpu2006_dir/tools/src/
+install_depency_centos() {
+	yum install -y gcc gcc-c++ gcc-gfortran numactl
 }
 
-# replace config.guess
-cp build/config.guess $cpu2006_dir/tools/src/specsum/build-aux/
-cp build/config.guess $cpu2006_dir/tools/src/tar-1.25/build-aux/
-cp build/config.guess $cpu2006_dir/tools/src/xz-5.0.0/build-aux/
-cp build/config.guess $cpu2006_dir/tools/src/rxp-1.5.0/
-cp build/config.guess $cpu2006_dir/tools/src/make-3.82/config/
+fix_prebuild() {
+	# replace config.guess
+	cp build/config.guess $cpu2006_dir/tools/src/specsum/build-aux/
+	cp build/config.guess $cpu2006_dir/tools/src/tar-1.25/build-aux/
+	cp build/config.guess $cpu2006_dir/tools/src/xz-5.0.0/build-aux/
+	cp build/config.guess $cpu2006_dir/tools/src/rxp-1.5.0/
+	cp build/config.guess $cpu2006_dir/tools/src/make-3.82/config/
 
-# modify Configure of perl
-grep -q "aarch64" $cpu2006_dir/tools/src/perl-5.12.3/Configure > /dev/null 2>&1
-if [ ! $? -eq 0 ]; then
-sed -i 'N;1324atest -d /lib/aarch64-linux-gnu && glibpth="$glibpth /lib/aarch64-linux-gnu /usr/lib/aarch64-linux-gnu"' $cpu2006_dir/tools/src/perl-5.12.3/Configure
-fi
+	# modify Configure of perl
+	grep -q "aarch64" $cpu2006_dir/tools/src/perl-5.12.3/Configure > /dev/null 2>&1
+	if [ ! $? -eq 0 ]; then
+	sed -i 'N;1324atest -d /lib/aarch64-linux-gnu && glibpth="$glibpth /lib/aarch64-linux-gnu /usr/lib/aarch64-linux-gnu"' $cpu2006_dir/tools/src/perl-5.12.3/Configure
+	fi
 
-# modify stdio.in.h
+	# modify stdio.in.h
 
-grep -q "//#undef" $cpu2006_dir/tools/src/tar-1.25/gnu/stdio.in.h
-if [ ! $? -eq 0 ]; then
-	sed -i '146s%#undef%//#undef%' $cpu2006_dir/tools/src/tar-1.25/gnu/stdio.in.h
-fi
+	grep -q "//#undef" $cpu2006_dir/tools/src/tar-1.25/gnu/stdio.in.h
+	if [ ! $? -eq 0 ]; then
+		sed -i '146s%#undef%//#undef%' $cpu2006_dir/tools/src/tar-1.25/gnu/stdio.in.h
+	fi
 
-grep -q "//_GL_WARN_ON_USE" $cpu2006_dir/tools/src/tar-1.25/gnu/stdio.in.h
-if [ ! $? -eq 0 ]; then
-	sed -i '147s%_GL_WARN_ON_USE%//_GL_WARN_ON_USE%' $cpu2006_dir/tools/src/tar-1.25/gnu/stdio.in.h
-fi
+	grep -q "//_GL_WARN_ON_USE" $cpu2006_dir/tools/src/tar-1.25/gnu/stdio.in.h
+	if [ ! $? -eq 0 ]; then
+		sed -i '147s%_GL_WARN_ON_USE%//_GL_WARN_ON_USE%' $cpu2006_dir/tools/src/tar-1.25/gnu/stdio.in.h
+	fi
 
-# modify stdio.in.h
+	# modify stdio.in.h
 
-grep -q "//#undef" $cpu2006_dir/tools/src/specsum/gnulib/stdio.in.h
-if [ ! $? -eq 0 ]; then
-	sed -i '161s%#undef%//#undef%' $cpu2006_dir/tools/src/specsum/gnulib/stdio.in.h
-fi
+	grep -q "//#undef" $cpu2006_dir/tools/src/specsum/gnulib/stdio.in.h
+	if [ ! $? -eq 0 ]; then
+		sed -i '161s%#undef%//#undef%' $cpu2006_dir/tools/src/specsum/gnulib/stdio.in.h
+	fi
 
-grep -q "//_GL_WARN_ON_USE" $cpu2006_dir/tools/src/specsum/gnulib/stdio.in.h
-if [ ! $? -eq 0 ]; then
-	sed -i '162s%_GL_WARN_ON_USE%//_GL_WARN_ON_USE%' $cpu2006_dir/tools/src/specsum/gnulib/stdio.in.h
-fi
+	grep -q "//_GL_WARN_ON_USE" $cpu2006_dir/tools/src/specsum/gnulib/stdio.in.h
+	if [ ! $? -eq 0 ]; then
+		sed -i '162s%_GL_WARN_ON_USE%//_GL_WARN_ON_USE%' $cpu2006_dir/tools/src/specsum/gnulib/stdio.in.h
+	fi
+}
 
-[ -f $cpu2006_dir/config/d05-2cpu.cfg ] || cp config/d05-2cpu.cfg $cpu2006_dir/config/
-
+fix_test_issue() {
 # make test report valid
 
-grep -q "F-fsigned-char" $cpu2006_dir/config/flags/Example-gcc4x-flags-revA.xml > /dev/null 2>&1
-if [ ! $? -eq 0 ]; then
-	sed -i 's%F-funsigned-char%F-fsigned-char%' $cpu2006_dir/config/flags/Example-gcc4x-flags-revA.xml
-	sed -i '/<\/flagsdescription>/d' $cpu2006_dir/config/flags/Example-gcc4x-flags-revA.xml
-	cat >> $cpu2006_dir/config/flags/Example-gcc4x-flags-revA.xml <<EOF
+	grep -q "F-fsigned-char" $cpu2006_dir/config/flags/Example-gcc4x-flags-revA.xml > /dev/null 2>&1
+	if [ ! $? -eq 0 ]; then
+		sed -i 's%F-funsigned-char%F-fsigned-char%' $cpu2006_dir/config/flags/Example-gcc4x-flags-revA.xml
+		sed -i '/<\/flagsdescription>/d' $cpu2006_dir/config/flags/Example-gcc4x-flags-revA.xml
+		cat >> $cpu2006_dir/config/flags/Example-gcc4x-flags-revA.xml <<EOF
 
 <flag name="F-fno-aggressive-loop-optimizations"
       class="optimization">
@@ -91,11 +83,41 @@ gnu90 portability
 ]]>
 </flag>
 
+<flag name="F-fpermissive"
+      class="portability">
+<example>
+-fpermissive
+</example>
+<![CDATA[
+<p> 
+cxx portability
+</p>
+]]>
+</flag>
+
 </flagsdescription>
 EOF
+	fi
+}
+
+mount | grep -q "$iso_file" > /dev/null 2>&1
+if [ ! $? -eq 0 ]; then
+	mount $iso_file $iso_mnt
 fi
 
+[ -d $cpu2006_dir ] || {
+	mkdir $cpu2006_dir && cp -a $iso_mnt/* $cpu2006_dir
+
+	# replace make
+	#rm -rf $cpu2006_dir/tools/src/make-3.82
+	#tar xf make.tar.gz -C $cpu2006_dir/tools/src/
+}
+
+[ -f $cpu2006_dir/config/d05-2cpu.cfg ] || cp config/d05-2cpu.cfg $cpu2006_dir/config/
+
 build_test() {
+	fix_prebuild
+	fix_test_issue
 	export FORCE_UNSAFE_CONFIGURE=1
 	cd $cpu2006_dir/tools/src
 	echo y | ./buildtools
@@ -106,9 +128,12 @@ run_test() {
 	cd $cpu2006_dir
 	. ./shrc
 
-	runspec -c d05-2cpu.cfg all --rate 1
-	runspec -c d05-2cpu.cfg all --rate 64
+	runspec -c d05-2cpu.cfg fp --rate 1 --output_format html,pdf 
+	#runspec -c d05-2cpu.cfg all --rate 1 --output_format html,pdf 
+	#runspec -c d05-2cpu.cfg all --rate 64 --output_format html,pdf 
 }
 
+install_depency_centos
+#install_depency_ubuntu
 build_test
 run_test
